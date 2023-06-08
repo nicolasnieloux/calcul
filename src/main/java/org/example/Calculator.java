0a76e5f6a43bb38c6426cc287818005117316c0a
Nicolas Nieloux <nicolas.nieloux@le-campus-numerique.fr>
2023-06-08 15:29:52 +0200

    fin du module

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,21 +2,24 @@ package org.example;
 
 public class Calculator {
 
-private int a;
-private int b;
+    private int a;
+    private int b;
 
+    private int result;
 
-public int add(int a, int b){
-        return a+b;
-}
+    public int add(int a, int b) {
+        return a + b;
+    }
 
-public String divise(int a, int b){
-        if (b==0){
-                return ("division par zero impossible");
+    public String divise(int a, int b) {
+        if (b == 0) {
+            return ("division par zero impossible");
+        } else {
+            return String.valueOf(a / b);
         }
-        else {
-                return String.valueOf(a/b);
-        }
-}
+    }
 
+    public int divise2(int a, int b) throws ArithmeticException {
+        return a/b;
+    }
 }
--- src/test/java/Calculator_test.java
+++ src/test/java/Calculator_test.java
@@ -2,28 +2,38 @@ import org.example.Calculator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.invoke.MethodHandles.catchException;
+
 public class Calculator_test {
 
-@Test
-    public void testAdd(){
+    @Test
+    public void testAdd() {
 
-   Calculator addition = new Calculator();
+        Calculator addition = new Calculator();
 
-   int expected = 6;
-   int actual = addition.add(4,2);
-   Assertions.assertEquals(expected,actual);
-}
+        int expected = 6;
+        int actual = addition.add(4, 2);
+        Assertions.assertEquals(expected, actual);
+    }
 
     @Test
-    public void testDiv(){
+    public void testDiv() {
 
         Calculator addition = new Calculator();
 
         String expected = "division par zero impossible";
-        String actual = addition.divise(4,0);
-        Assertions.assertEquals(expected,actual);
+        String actual = addition.divise(4, 0);
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testDivByZero() {
 
+        Calculator addition = new Calculator();
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            addition.divise2(4, 0);
+        });
+
+    }
 
 }
